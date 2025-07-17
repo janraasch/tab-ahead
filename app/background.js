68ede25d18b22328984220655ebd220075887d67
Jan Raasch <jan@janraasch.com>
2025-07-17 18:06:14 +0200

    Add comment :)

--- app/background.js
+++ app/background.js
@@ -9,3 +9,11 @@ console.log('Tab Ahead service worker loaded');
 
 // Optional: Add any background functionality here if needed in the future
 // For example, listening for tab events, managing extension state, etc. 
+
+// TODO: Not sure if this is needed anymore. On manifest v2, we used a bg.html file:
+// This is really just to render some
+// background page forcing Chrome to
+// preload the plugin code such that the
+// popup opens quickly later on.
+// See http://stackoverflow.com/questions/20139744/browseraction-opens-slowly-unless-already-open-in-a-tab
+// and http://stackoverflow.com/questions/26276815/my-chrome-extension-popup-opens-after-a-few-seconds-its-slow-compared-to-other
